59538096fd95844bbfbe7c7616b6d885a2f2ba6e
unknown <topxeq@163.com>
2021-03-01 15:21:29 +0800

    2021030101

---
+++ blockix.go
@@ -0,0 +1,8 @@
+package blockix
+
+import "fmt"
+
+func Run(scriptA string, inputA string) string {
+
+	return fmt.Sprintf("%v\n", "test")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/topxeq/blockix
+
+go 1.16
